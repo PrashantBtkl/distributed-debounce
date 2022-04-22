bff0aafcfd8ed51c7c6ac91da218f07439cc5e05
PrashantBtkl <prashant44backup@gmail.com>
2022-04-22 15:29:17 +0530

    initial commit

---
+++ debouncer/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"github.com/PrashantBtkl/distributed-debounce/debouncer/mq"
+	"github.com/apex/log"
+	"github.com/apex/log/handlers/cli"
+	"github.com/jinzhu/configor"
+)
+
+func run() {
+	log.SetHandler(cli.Default)
+	log.SetLevel(log.DebugLevel)
+
+	var config Config
+
+	err := configor.Load(&config, "config/config.json")
+	if err != nil {
+		log.Fatalf("run: failed to init config: %v", err)
+	}
+
+	rmq, err := mq.initRabbitMQ(config.AMQP)
+	if err != nil {
+		log.Fatalf("run: failed to init rabbitmq: %v", err)
+	}
+	defer rmq.Shutdown()
+
+	err = rmq.PublishWithDelay("user.event.publish", []byte("hello"), 10000)
+	if err != nil {
+		log.Fatalf("run: failed to publish into rabbitmq: %v", err)
+	}
+
+	for {
+	}
+}
+
+func main() {
+	run()
+}
